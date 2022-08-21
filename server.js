cf298831c142b54dd4fbc29b2191643708198dcb
kekule15 <onyekachiaugustus111@gmail.com>
2022-08-21 03:46:10 +0100

    update

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 const express = require("express");
 require("dotenv").config();
-const cloudinary = require("./middleware/cloudinary");
-const upload = require("./middleware/multer");
-const cors = require("cors");
-
 const db = require("./config/db-config");
 
 const app = express();
+
+
 app.use(express.json());
 
-var corOptions = {
-  origin: "https://localhost:8081",
-};
-
-app.use(cors(corOptions));
 app.use(express.urlencoded({ extended: true }));
 
 // port
@@ -27,29 +20,13 @@ db.connect(function (err) {
   console.log("Connected!");
 });
 
-app.post("/uploadImage", upload.single("image"), async(req, res) => {
-  try{
-      const result = await cloudinary.uploader.upload(req.file.path);
-     const url = result.url;
-     console.log(url);
-      res.json({
-        data: {
-          status: "done",
-          name: req.body.name
-        },
-        result : result
-      })
-  }catch(err){
-      console.log(err);
-  }
-})
 
-// app.use('/api/auth', userRouter)
+// routes
 require("./routes/user_vendor_routes")(app);
 require("./routes/blogpost_routes")(app);
 require("./routes/gift_routes")(app);
 
-//testing api
+
 
 //server
 app.listen(PORT, (err) => {
